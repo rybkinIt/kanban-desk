93ac1372b449f30aefa61e7a9363a3ec6652d569
rybkinIt <116676756+rybkinIt@users.noreply.github.com>
2023-02-21 14:08:04 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () =>{
                 <p className='footer-title title2'>Finished tasks {input.filter(item => item.status === 4).length}</p>
             </div>
                 <div className='info-container'>
-                    <p className='footer-title title3'>Kanban board by &lt;NAME&gt;, &lt;{Date.year}&gt;</p>
+                    <p className='footer-title title3'>Kanban board by &lt;NAME&gt;, &lt;{Date}&gt;</p>
                 </div>
         </div>
     )
